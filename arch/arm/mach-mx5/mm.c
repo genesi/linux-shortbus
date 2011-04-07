e4d4321f7c48ad3b9c87568318fd6575f5fea125
Zeng Zhaoming <b32542@freescale.com>
2011-04-07 09:16:51 +0800

    ENGR00141732 SDMA: Fix bootup stop at sdma firmware loading
    Linux 2.6.38 kernel bootup slow, it stopped at sdma firmware
    loading for about one minute.
    
    It is caused by firmware store on rootfs lib/firmware directory,
    while sdma initialization happens before rootfs mounted.
    
    To fix it, we have to build firmware in kernel image.
    Signed-off-by: Zeng Zhaoming <b32542@freescale.com>
    Signed-off-by: Eric Miao <eric.miao@linaro.org>

--- arch/arm/mach-mx5/mm.c
+++ arch/arm/mach-mx5/mm.c
@@ -132,7 +132,7 @@ static struct sdma_script_start_addrs imx51_sdma_script __initdata = {
 };
 
 static struct sdma_platform_data imx51_sdma_pdata __initdata = {
-	.fw_name = "sdma-imx51.bin",
+	.fw_name = "imx/sdma/sdma-imx51-to3.bin",
 	.script_addrs = &imx51_sdma_script,
 };
 
@@ -151,7 +151,7 @@ static struct sdma_script_start_addrs imx53_sdma_script __initdata = {
 };
 
 static struct sdma_platform_data imx53_sdma_pdata __initdata = {
-	.fw_name = "sdma-imx53.bin",
+	.fw_name = "imx/sdma/sdma-imx53-to1.bin",
 	.script_addrs = &imx53_sdma_script,
 };
 
