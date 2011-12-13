b2eed1e031a72d60cf5f03e4370b8471e4beea33
Josef Bacik <josef@redhat.com>
2011-12-13 16:04:54 -0500

    Btrfs: only set cache_generation if we setup the block group
    A user reported a problem booting into a new kernel with the old format inodes.
    He was panicing in cow_file_range while writing out the inode cache.  This is
    because if the block group is not cached we'll just skip writing out the cache,
    however if it gets dirtied again in the same transaction and it finished caching
    we'd go ahead and write it out, but since we set cache_generation to the transid
    we think we've already truncated it and will just carry on, running into
    cow_file_range and blowing up.  We need to make sure we only set
    cache_generation if we've done the truncate.  The user tested this patch and
    verified that the panic no longer occured.  Thanks,
    
    Reported-and-Tested-by: Klaus Bitto <klaus.bitto@gmail.com>
    Signed-off-by: Josef Bacik <josef@redhat.com>

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -2749,6 +2749,8 @@ out_free:
 	btrfs_release_path(path);
 out:
 	spin_lock(&block_group->lock);
+	if (!ret && dcs == BTRFS_DC_SETUP)
+		block_group->cache_generation = trans->transid;
 	block_group->disk_cache_state = dcs;
 	spin_unlock(&block_group->lock);
 
