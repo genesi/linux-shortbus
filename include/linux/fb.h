50d767532315aee19be391a879d8dbf7ae129746
Steev Klimaszewski <steev@genesi-usa.com>
2012-01-03 14:53:59 -0600

    fb: Add ifdef for siihdmi

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -1163,7 +1163,11 @@ struct fb_videomode {
 
 extern const char *fb_mode_option;
 extern const struct fb_videomode vesa_modes[];
+#if defined(CONFIG_FB_MXC_SIIHDMI)
 extern const struct fb_videomode cea_modes[65];
+#else
+extern const struct fb_videomode cea_modes[64];
+#endif
 
 struct fb_modelist {
 	struct list_head list;
